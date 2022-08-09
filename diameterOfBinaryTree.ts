037fad38b469fe40a224f0868a355622b66d0b2d
xiaosiyier <xiaosiyier@163.com>
2022-08-09 21:39:16 +0800

    go to bedzzzzzz

---
+++ diameterOfBinaryTree.ts
@@ -0,0 +1,15 @@
+import TreeNode from "./TreeNode";
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  let ans = 0;
+  const dfs: (root: TreeNode | null) => number = (root) => {
+    if (!root) {
+      return 0;
+    }
+    const left = dfs(root.left),
+      right = dfs(root.right);
+    ans = Math.max(ans, left + right);
+    return 1 + Math.max(left, right);
+  };
+  dfs(root);
+  return ans;
+}
---
+++ inorderTraversal.ts
@@ -0,0 +1,14 @@
+import TreeNode from "./TreeNode";
+function inorderTraversal(root: TreeNode | null): number[] {
+  const res: number[] = [];
+  const inorder = (root: TreeNode | null) => {
+    if (!root) {
+      return;
+    }
+    inorder(root.left);
+    res.push(root.val);
+    inorder(root.right);
+  };
+  inorder(root);
+  return res;
+}
